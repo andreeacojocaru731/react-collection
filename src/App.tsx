37be1d10f71ba160ca4847f6bf00aa457956b55f
Andreea Mocanu <gabrielaandreea25@yahoo.com>
2026-03-06 16:20:07 +0200

    Add react lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Style from "./App.module.scss";
-import Focus from "./components/focus/focus";
-import MegaStoreApp from "./components/mega-store-app/mega-store-app";
 import cx from "classnames";
 import HomePage from "./components/home-page/home-page";
-import Login from "./auth/login/login";
 import Layout from "./components/layout/layout";
 import { UserContext } from "./state-manager/userContext";
 import { useState } from "react";
+import React from "react";
+
+const Login = React.lazy(() => import("./auth/login/login"));
+const Focus = React.lazy(() => import("./components/focus/focus"));
+const MegaStoreApp = React.lazy(
+  () => import("./components/mega-store-app/mega-store-app"),
+);
 
 function App() {
   const [userData, setUserData] = useState({ token: "", isLoggedIn: false });
