170d1eab793fc6fcafaa891c40fa972292b5bd5f
Andreea Mocanu <gabrielaandreea25@yahoo.com>
2026-03-08 17:04:13 +0200

    Refresh time on home page

--- src/components/home-page/home-page.tsx
+++ src/components/home-page/home-page.tsx
@@ -2,16 +2,27 @@ import { useTranslation } from "react-i18next";
 import Unauthorized from "../../auth/login/unauthorized";
 import cx from "classnames";
 import Style from "./home-page.module.scss";
-import { UserContext } from "../../state-manager/userContext";
-import { useContext } from "react";
+import { useEffect, useState } from "react";
+// import { UserContext } from "../../state-manager/userContext";
+// import { useContext } from "react";
 
 export default function HomePage() {
   const { t } = useTranslation("common");
-  const { userData } = useContext(UserContext);
+  // const { userData } = useContext(UserContext);
+  const token = localStorage.getItem("token");
+
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [currentDate]);
 
   return (
     <>
-      {userData.isLoggedIn ? (
+      {token ? (
         <>
           <h3 className={cx(Style.description)}>{t("homeDescription")}</h3>
 
@@ -20,7 +31,7 @@ export default function HomePage() {
 
           <div>
             {t("intlDateTime", {
-              val: new Date(),
+              val: currentDate,
               formatParams: {
                 val: {
                   weekday: "long",
